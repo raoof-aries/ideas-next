a34d7049872fc074956cc36f3d409be9d163ae69
raoof-aries <raoof@ariesesolutions.com>
2026-02-26 09:49:44 +0530

    fix: add dynamic export to robots and sitemap, and create .htaccess for URL handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      // Remove trailing slashes
-      {
-        source: "/:path+/",
-        destination: "/:path+",
-        permanent: true,
-      },
-      // Redirect non-www to www
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "host",
-            value: "ideas.ariesmar.com",
-          },
-        ],
-        destination: "https://www.ideas.ariesmar.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  output: "export",
+
   images: {
     formats: ["image/webp"],
   },
